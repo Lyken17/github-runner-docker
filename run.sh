0334bddf059ba4e0c32178c43e9ea2bd9ae3edc2
Ligeng Zhu <Lyken17@users.noreply.github.com>
2025-08-28 23:04:23 +0800

    Update run.sh and srun_launch.sh: Refactor environment variable exports and adjust runner configurations for improved clarity and functionality.

--- run.sh
+++ run.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
-export RUNNER_REPOSITORY_URL=https://github.com/Efficient-Large-Model/flash-attention-builder
 
 docker build --platform linux/amd64 -t test-runner .
 docker tag test-runner lyken/test-runner:latest
 docker push lyken/test-runner:latest
 
-# export RUNNER_TOKEN=xxx
-# export RUNNER_REPOSITORY_URL=https://github.com/Efficient-Large-Model/flash-attention
-echo "RUNNER_TOKEN: $RUNNER_TOKEN"
-# echo "RUNNER_REPOSITORY_URL: $RUNNER_REPOSITORY_URL"
-# docker run -it --rm \
-#     --name github-actions-runner \
-#     -e RUNNER_TOKEN=$RUNNER_TOKEN \
-#     -e RUNNER_REPOSITORY_URL=$RUNNER_REPOSITORY_URL \
-#     test-runner
+export RUNNER_REPOSITORY_URL=https://github.com/Efficient-Large-Model/flash-attention-builder
+export RUNNER_ORGANIZATION_URL=https://github.com/Efficient-Large-Model
+export ELM_RUNNER_TOKEN=$ELM_RUNNER_TOKEN
+export RUNNER_TAGS="self-hosted,linux,x64,gpu"
+export RUNNER_NAME="ligeng-local-mact-test"
+# export RUNNER_TAGS=${RUNNER_TAGS:-"self-hosted,linux,x64,gpu,docker"} 
+export RUNNER_TAGS=${RUNNER_TAGS:-"self-hosted,linux,x64,docker"} 
 
 docker run -it --rm \
     --name github-actions-runner \
-    -e GITHUB_ACCESS_TOKEN=$GIT_REPO_TOKEN \
+    -e GITHUB_ACCESS_TOKEN=$ELM_RUNNER_TOKEN \
     -e RUNNER_REPOSITORY_URL=$RUNNER_REPOSITORY_URL \
-    -e RUNNER_TAGS="self-hosted,linux,x64,cpu-small" \
-    -e RUNNER_NAME="ligeng-mac" \
+    -e RUNNER_ORGANIZATION_URL=$RUNNER_ORGANIZATION_URL \
+    -e RUNNER_TAGS=$RUNNER_TAGS \
+    -e RUNNER_NAME=$RUNNER_NAME \
     test-runner
--- srun_launch.sh
+++ srun_launch.sh
@@ -1,11 +1,12 @@
 while true; do
 
 GITHUB_ACCESS_TOKEN=$GIT_REPO_TOKEN \
-RUNNER_REPOSITORY_URL=$RUNNER_REPOSITORY_URL \
+RUNNER_REPOSITORY_URL=https://github.com/Efficient-Large-Model \
 RUNNER_TAGS="self-hosted,linux,x64,gpu" \
 RUNNER_NAME="ligeng-nrt-gpu" \
+RUNNER_TAGS=${RUNNER_TAGS:-"self-hosted,linux,x64,gpu,docker"} \
     srun -A $VILA_SLURM_ACCOUNT -p interactive,$VILA_SLURM_PARTITION -t 4:00:00 \
-    -J dev:dockertest \
+    -J dev:docker-group-ci \
     --gpus-per-node 8 --exclusive \
     --no-container-remap-root --no-container-mount-home \
     --container-image lyken/test-runner \
@@ -13,3 +14,23 @@ RUNNER_NAME="ligeng-nrt-gpu" \
 
 sleep 10
 done
+
+while true; do
+
+GITHUB_ACCESS_TOKEN=$ELM_RUNNER_TOKEN \
+RUNNER_ORGANIZATION_URL=https://github.com/Efficient-Large-Model \
+RUNNER_TAGS="self-hosted,linux,x64,gpu" \
+RUNNER_NAME="ligeng-cs-gpu" \
+RUNNER_TAGS=${RUNNER_TAGS:-"self-hosted,linux,x64,gpu,docker,a100"} \
+    srun -A $VILA_SLURM_ACCOUNT -p interactive,$VILA_SLURM_PARTITION -t 4:00:00 \
+    -J dev:docker-group-ci \
+    --gres=gpu:1 --cpus-per-task 16 --mem=256G \
+    --no-container-remap-root --no-container-mount-home \
+    --container-image lyken/test-runner \
+    --pty bash start.sh
+
+sleep 10
+done
+
+
+VILA_SLURM_PARTITION=interactive,$VILA_SLURM_PARTITION eai-run --pty bash
